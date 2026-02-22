d2b3680ab5b0a488de8e6e61786b13777224338e
KR-ISCODE <krisjenxz@gmail.com>
2026-02-22 14:55:13 +0700

    [feature]:setup migration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
+using final_project.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+builder.Services.AddControllers();
 
 // Add services to the container.
 
